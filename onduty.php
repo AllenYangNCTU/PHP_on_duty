3b1c011767f367cbb63e3b47d45f838de4db0a03
Kamigami85 <yanghejie.bt04@nycu.edu.tw>
2022-05-24 20:45:33 +0800

    change index.html

--- onduty.php
+++ onduty.php
@@ -5,12 +5,4 @@ shuffle($duty);
 for($i=0;$i<22;$i=$i+2){print($duty[$i].",".$duty[$i+1]."<br>");}
 shuffle($duty);
 for($i=0;$i<22;$i=$i+2){print($duty[$i].",".$duty[$i+1]."<br>");}
-print("打掃<br>");
-$duty=array(2,3,4,6,7,8,9,11,12,13,14,15,16,18,19,20,21,23,24,25);
-shuffle($duty);
-shuffle($duty);
-shuffle($duty);
-shuffle($duty);
-for($i=0;$i<20;$i++){print($duty[$i]."<br>");}
-
 ?>
